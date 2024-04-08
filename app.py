482779aa6da8a4ff7a4ab59ebf11d2cc3cbf0b37
Leleco1604 <leonardogbarbosamelo2003@gmail.com>
2024-04-08 17:24:47 -0300

    commit

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ import openpyxl
 driver = webdriver.Chrome()
 driver.get('https://www.novaliderinformatica.com.br/computadores-gamers')
 
-titulos = driver.find_element(By.XPATH, "//a[@class='nome-produto']")
-preco = driver.find_element(By.XPATH, "")
-
 # extrair todos os titulos
+titulos = driver.find_element(By.XPATH, "//a[@class='nome-produto']")
 # estrair todos os preços
+precos = driver.find_element(By.XPATH, "//strong[@class='preco-promocional']")
 # inserir tudo em uma planilha 
+#zip: faz com que o o for so percorra itens que estão nas duas listas
+for titulo, preco in zip(titulos, precos): 
+    pass 
 
