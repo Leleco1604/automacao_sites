7408f67469e7cac0b463720ea346d30b9a952de1
Leleco1604 <leonardogbarbosamelo2003@gmail.com>
2024-04-09 12:42:42 -0300

    modificações

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By #acessar informações do site 
+from selenium.webdriver.common.by import By
 import openpyxl
 
-
-# acessar site https://www.novaliderinformatica.com.br
+# acessar o site https://www.novaliderinformatica.com.br/computadores-gamers
 driver = webdriver.Chrome()
 driver.get('https://www.novaliderinformatica.com.br/computadores-gamers')
 
-# extrair todos os titulos
-titulos = driver.find_element(By.XPATH, "//a[@class='nome-produto']")
-# estrair todos os preços
-precos = driver.find_element(By.XPATH, "//strong[@class='preco-promocional']")
+# extrair todos os títulos
+titulos = driver.find_elements(By.XPATH,"//a[@class='nome-produto']")
+# extrair todos os preços
+precos = driver.find_elements(By.XPATH,"//strong[@class='preco-promocional']")
+
+# Criando a planilha
+workbook = openpyxl.Workbook()
+# Criando a página 'produtos'
+workbook.create_sheet('produtos')
+# Seleciono a página produtos
+sheet_produtos = workbook['produtos']
+sheet_produtos['A1'].value = 'Produto'
+sheet_produtos['B1'].value = 'Preço'
+
 # inserir tudo em uma planilha 
 #zip: faz com que o o for so percorra itens que estão nas duas listas
-for titulo, preco in zip(titulos, precos): 
-    pass 
+for titulo, preco in zip(titulos, precos):
+    sheet_produtos.append([titulo.text,preco.text])
+
+workbook.save('produtos.xlsx')
+
 
